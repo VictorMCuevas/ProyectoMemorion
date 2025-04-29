c83e7597710576004e99a9e335bcef84bcf9446f
Víctor M. Bartolomé <victorcuevas1790@gmail.com>
2025-04-29 19:36:58 +0200

    Cabio en Script

--- script.js
+++ script.js
@@ -11,24 +11,11 @@ btnInsertar.addEventListener('click', () => {
     }
     const tablero = document.createElement('div');
     tablero.classList.add("tablero");
-    tarjeta.style.backgroundColor = fondo;
-    tarjeta.style.color = colText;
-    tarjeta.style.fontFamily = fuente;
-    tarjeta.style.padding='10px';
-    tarjeta.style.margin='10px 5';
-    tarjeta.style.marginLeft = '500px';
-    tarjeta.style.marginRight = '500px';
-    tarjeta.style.borderRadius = '8px';
 
-    tarjeta.innerHTML  = `
-        <h2>${nombre}</h2>
-        <h4>Puesto: ${puesto}</h4>
+    tablero.innerHTML  = `
+        <h2>¡Suerte ${nombre} !</h2>
     `;
     const btnEliminar = document.createElement("button")
-
-    btnEliminar.addEventListener("click", () => {
-        tarjeta.remove()
-    })
 
     btnEliminar.textContent = "🗑";
     tarjeta.appendChild(btnEliminar);
