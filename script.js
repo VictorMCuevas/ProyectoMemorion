2eae53396caedadbd9d096bc8edb6192141127dd
Víctor M. Bartolomé <victorcuevas1790@gmail.com>
2025-04-29 19:31:40 +0200

    Cabios nuevos

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-const iniciar = document.getElementById("iniciar");
-const nombre = document.getElementById("nombre");
-const modo = document.getElementsByClassName("modo");
+const btnInsertar = document.getElementById("iniciar");
+
+btnInsertar.addEventListener('click', () => {
+    const nombre = document.getElementById("nombre").value;
+    const dificultad = document.getElementById("dimensiones").value;
+    const tema = document.getElementById("tema").value;
+
+    if(nombre==="" || dificultad===""||tema ===""){
+        alert("Por favor, rellene los campos");
+        return;
+    }
+    const tablero = document.createElement('div');
+    tablero.classList.add("tablero");
+    tarjeta.style.backgroundColor = fondo;
+    tarjeta.style.color = colText;
+    tarjeta.style.fontFamily = fuente;
+    tarjeta.style.padding='10px';
+    tarjeta.style.margin='10px 5';
+    tarjeta.style.marginLeft = '500px';
+    tarjeta.style.marginRight = '500px';
+    tarjeta.style.borderRadius = '8px';
+
+    tarjeta.innerHTML  = `
+        <h2>${nombre}</h2>
+        <h4>Puesto: ${puesto}</h4>
+    `;
+    const btnEliminar = document.createElement("button")
+
+    btnEliminar.addEventListener("click", () => {
+        tarjeta.remove()
+    })
+
+    btnEliminar.textContent = "🗑";
+    tarjeta.appendChild(btnEliminar);
+    tarjetas.appendChild(tarjeta);
+    //console.log(`Fuente seleccionada ${nombre}`);
+});
