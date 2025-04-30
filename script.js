a2d2ee40b16f0f0e4245e6cec05ca187cf42e1f5
Apocaliptico70 <thebufjunior@gmail.com>
2025-04-30 19:02:38 +0200

    +

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const btnInsertar = document.getElementById("iniciar");
 const inicio = document.getElementById("inicio");
 const tablero = document.getElementById("tablero");
+const menuPedirMedidas = document.getElementById("menuPedirMedidas");
+let ancho = document.getElementById("ancho");
+let alto = document.getElementById("alto")
+
+dimensiones.addEventListener('click', () =>{
+    if(dimensiones.value == "Personalizado"){
+        menuPedirMedidas.style.display = "block";
+    }
+    if(dimensiones.value != "Personalizado"){
+        menuPedirMedidas.style.display = "none";
+        if(dimensiones === "Fácil (4x4)"){
+            alto = "4";
+            ancho = "4";
+        }else if( dimensiones=== "Medio (5x4)"){
+            alto = "4";
+            ancho = "5"; 
+        }else if( dimensiones === "Difícil (6x6)"){
+            alto.value = "6";
+            ancho = "6";
+        }
+    }
+});
+
 
 btnInsertar.addEventListener('click', () => {
     const nombre = document.getElementById("nombre").value;
@@ -10,11 +33,14 @@ btnInsertar.addEventListener('click', () => {
     if(nombre==="" || dimensiones===""||tema ===""){
         alert("Por favor, rellene los campos");
         return;
+
     }else {
         inicio.style.display = "none";
         tablero.style.display = "block";
     }
 });
+
+
 window.addEventListener("DOMContentLoaded", () => {
     const tablero = document.getElementById("tablero");
     tablero.classList.add("animar-tablero");
