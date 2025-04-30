b58854316c19a7e0d37f55c522ee4a40525c1e6c
daylin12212 <carolinagomezvelez753@gmail.com>
2025-04-30 17:23:44 +0200

    nuevo

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ btnInsertar.addEventListener('click', () => {
     document.body.appendChild(tablero);
     const url = `pantalla.html?nombre=${encodeURIComponent(nombre)}&dimensiones=${encodeURIComponent(dimensiones)}&tema=${encodeURIComponent(tema)}`;
     window.open(url, "_blank");
+
+    let btnFormulario = document.getElementById("iniciar");
+    let btnCarta = document.getElementById("carta");
+    let btnHorizontal = document.getElementById("horizontal");
+    let contenidoFacil = document.getElementById("Fácil (4x4)").textContent
+    let contenidoMedio = document.getElementById("Medio (5x4)").textContent
+    let contenidoDificil = document.getElementById("Difícil (6x6)").textContent
+
+
+    btnFormulario.addEventListener('click', () => {
+    if(&dimensiones == contenidoFacil){
+        for(let i = 1; i <= 4; i++){
+            let btnCarta = document.createElement('carta');
+            let btnHorizontal = document.createElement('horizontal');
+        }
+    }
+})
 });
+
